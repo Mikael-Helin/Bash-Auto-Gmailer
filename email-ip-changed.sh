23976991d938fe090fb3e3f3fe215b33b290d2c9
Mikael <mikael@tuf17.mikaelhelin.com>
2024-09-29 13:39:09 +0200

    update

--- email-ip-changed.sh
+++ email-ip-changed.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Enable debugging (optional, can be removed later)
-set -x
+# set -x
 
+# Set the data directory and files
 DATA_DIR="/opt/myip/data"
 DATA_FILE="$DATA_DIR/last_ip.txt"
 EMAIL_FILE="$DATA_DIR/email.txt"
@@ -61,26 +62,30 @@ get_last_ip() {
     echo "$IP"
 }
 
-# Check if the data file exists
+# If last IP file does not exist then create it
 if [ ! -f "$DATA_FILE" ]; then
     echo "Creating $DATA_FILE"
     LAST_IP=$(get_current_ip)
     LAST_IP=$(trim "$LAST_IP")
     echo "$LAST_IP" > "$DATA_FILE" || { echo "Failed to create $DATA_FILE"; exit 1; }
-
-    echo -e "Subject: IP Address for $HOSTNAME\n\nThe current IP address is: $LAST_IP" | /usr/sbin/ssmtp -v "$EMAIL"
-    if [ $? -ne 0 ]; then
-        echo "Failed to send email"
-        exit 1
-    fi
 fi
 
 while true; do
+    # Get the current IP, if it is empty then wait for 1 hour and try again
     CURRENT_IP=$(get_current_ip)
-    echo "Current IP: $CURRENT_IP"
+    if [ -z "$CURRENT_IP" ]; then
+        sleep 3600
+        continue
+    fi
+    # Get the last IP, if it is empty then insert the current IP and wait for 1 hour
     LAST_IP=$(get_last_ip)
-    echo "Last IP: $LAST_IP"
-
+    if [ -z "$LAST_IP" ]; then
+        echo "Inserting current IP into $DATA_FILE"
+        echo "$CURRENT_IP" > "$DATA_FILE" || { echo "Failed to write to $DATA_FILE"; exit 1; }
+        sleep 3600
+        continue
+    fi
+    # If the current IP is different from the last IP, then update the last IP and send an email
     if [ "$CURRENT_IP" != "$LAST_IP" ]; then
         echo "IP has changed. Updating $DATA_FILE"
         echo "$CURRENT_IP" > "$DATA_FILE" || { echo "Failed to write to $DATA_FILE"; exit 1; }
@@ -90,9 +95,6 @@ while true; do
             echo "Failed to send email"
             exit 1
         fi
-    else
-        echo "IP has not changed"
     fi
-
     sleep 3600
 done
